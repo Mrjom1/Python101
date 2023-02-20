779d1b4acfb0fa32935610d8020048fd5823e261
Shinamon <125757595+Mrjom1@users.noreply.github.com>
2023-02-20 22:36:57 +0700

    Add files via upload

---
+++ EP1/HW1_tao.py
@@ -0,0 +1,31 @@
+Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import turlte
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    import turlte
+ModuleNotFoundError: No module named 'turlte'
+>>> import turtle
+>>> tao = turtle.Pen()
+>>> tao = turtle.Pen()
+>>> 
+>>> for i in range(18):
+...     tao.left(20)
+...     for c in ["red", "green", "blue"]:
+...         tao.color(c)
+...         for p in range(6):
+...             tao.fd(80)
+...             tao.left(60)
+... 
+...             
+>>> tao.home()
+>>> tao.clear()
+>>> for i in range(12):
+...     for c in ['red', 'green', 'blue']:
+...         tao.left(30)
+...         tao.color(c)
+...         for p in range(6):
+...             tao.fd(80)
+...             tao.left(60)
+... 
+...             
---
+++ EP1/gui_knowledge.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+GUI = Tk ()
+GUI.title('โปรแกรมบันทึกข้อมูล')
+GUI.geometry('500x300')
+
+B1 = Button(GUI, text='มีเงินกี่บาท')
+B1.pack()
+
+GUI.mainloop()
